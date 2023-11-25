19ecddc06ce3a13465325e383813cca0a118c3d4
hector <hectorrrivera1010@gmail.com>
2023-11-25 01:01:14 -0600

    askdfljslakjf klasjfdl

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Param } from '@nestjs/common';
 
-@Controller()
+@Controller('greet')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get(':name')
+  getGreeting(@Param('name') name: string): string {
+    return `Hello ${name} its death grips`;
   }
 }
