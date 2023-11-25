bd006e5707bff9efaccb6366122819b726f2b6e6
hector <hectorrrivera1010@gmail.com>
2023-11-25 01:31:15 -0600

    [add] user Get, post and put methods

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,38 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Body,
+  Param,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 
-@Controller('greet')
+@Controller('user')
 export class AppController {
-  @Get(':name')
-  getGreeting(@Param('name') name: string): string {
-    return `Hello ${name} its death grips`;
+  private users: any[] = [];
+
+  @Get()
+  getUsers(): any[] {
+    return this.users;
+  }
+
+  @Post()
+  createUser(@Body() newUser: any): string {
+    if (!newUser.username || !newUser.email) {
+      throw new HttpException(
+        'Username and email are required fields',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    this.users.push(newUser);
+    return 'User created successfully';
+  }
+
+  @Put(':id')
+  updateUser(@Param('id') id: number, @Body() updatedUser: any): string {
+    this.users[id] = updatedUser;
+    return `User with id ${id} was updated successfully`;
   }
 }
